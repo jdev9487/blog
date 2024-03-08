ec161b7812d378fcd7f8a12e65979848038c4ed9
John Gould <johngould@Johns-MBP.lan>
2024-03-08 15:57:24 +0100

    mathjax

--- gatsby-config.js
+++ gatsby-config.js
@@ -114,5 +114,13 @@ module.exports = {
         icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
       },
     },
+    {
+      resolve: `gatsby-transformer-remark`,
+      options: {
+        plugins: [
+          `gatsby-remark-mathjax`,
+        ],
+      },
+    }
   ],
 }
