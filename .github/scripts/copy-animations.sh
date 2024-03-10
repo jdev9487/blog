31839a18a84fe6dea23c4466f967ec007c9c863b
John Gould <johngould9487@protonmail.com>
2024-03-10 10:05:07 +0100

    done

--- .github/scripts/copy-animations.sh
+++ .github/scripts/copy-animations.sh
@@ -1,9 +1,11 @@
 for path in ./content/blog/**/*.md
 do
+    echo $path
     parent_dir="$(dirname -- "$(realpath -- path)")"
+    echo $parent_dir
     grep -E -o "\w+\.gif" $path | while read -r file
     do
-        echo "cp ./animations/$file ./content/blog/$parent_dir/$file"
-        cp ./animations/$file ./content/blog/$parent_dir/$file
+        echo "cp ./animations/$file $parent_dir/$file"
+        cp ./animations/$file $parent_dir/$file
     done
 done
