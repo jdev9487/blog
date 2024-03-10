f9838aac5e7974d10aac38f3845eed0ff51dbf41
John Gould <johngould9487@protonmail.com>
2024-03-10 09:43:54 +0100

    done

--- .github/scripts/copy-animations.sh
+++ .github/scripts/copy-animations.sh
@@ -6,7 +6,7 @@ do
     echo $path
     grep -o *.gif $path | while read -r file
     do
-        echo "${file} - ${filename}/${file}
+        echo "${file} - ${filename}/${file}"
         cp ./animations/$file ./content/blog/$filename/$file
     done
 done
