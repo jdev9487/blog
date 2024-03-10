bb6fbf251e9311fb17a9e51e94f2db01a7e54e7b
John Gould <johngould9487@protonmail.com>
2024-03-10 09:54:16 +0100

    grep update

--- .github/scripts/copy-animations.sh
+++ .github/scripts/copy-animations.sh
@@ -4,7 +4,7 @@ do
     filename="${fullfilename%.*}"
     echo "-------------"
     echo $path
-    grep -o *.gif $path | while read -r file
+    grep -E -o "\w+\.gif" $path | while read -r file
     do
         echo "${file} - ${filename}/${file}"
         cp ./animations/$file ./content/blog/$filename/$file
