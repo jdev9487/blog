5d8754db4683bbe742a1f3a0de8338f23bffc6a3
John Gould <johngould9487@protonmail.com>
2024-03-21 18:16:44 +0100

    ngnix conf

--- .nginx/convert-nginx.sh
+++
@@ -1,5 +0,0 @@
-# !/usr/bin/env bash
-
-envsubst '$${NEXTJS_CONTAINER_IP}' < /etc/nginx/conf.d/holder.conf > /etc/nginx/conf.d/default.conf 
-rm -rf /etc/nginx/conf.d/holder.conf
-nginx -g "daemon off;"
