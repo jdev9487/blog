290904429398bfad7ebbcc10fc1dd9ef2fad9e1a
John Gould <johngould9487@protonmail.com>
2024-08-04 15:10:21 +0100

    rudimentary home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,25 +1,6 @@
-import Link from "next/link";
-// import { PostMetadata } from "./Components/postMetadata";
-// import PostPreview from "./Components/PostPreview";
-
 export default async function Page() {
-  // const res = await fetch(`${process.env.MARKDOWN_URL}/front-matter`, {
-  //   cache: "no-store"
-  // })
-  // let mattersJson = JSON.stringify(await res.json())
-  // let posts: PostMetadata[] = JSON.parse(mattersJson)
-  // const previews = posts.map(p => {
-  //   return(
-  //     <PostPreview key={p.slug} {...p}/>
-  //   )
-  // })
   return (
     <div>
-      <Link href={`/posts`}>Animated</Link>
-      <Link href={`/in-depth`}>In Depth</Link>
     </div>
-    // <div className="flex flex-col gap-4 mt-4">
-    //   {previews}
-    // </div>
   )
 }
